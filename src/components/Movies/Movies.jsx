0ff1d12b5baf26a92a57f1516c648cb760645ee3
Aksultan <aksultan.zhumadil@gmail.com>
2021-06-02 13:09:35 +0600

    fix: pagination bug on value reset

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -7,20 +7,23 @@ import Pagination from '@material-ui/lab/Pagination';
 
 function Movies(){
     let [movies, setMovies] = useState([]);
-    let [currentPage, setCurrentPage] = useState(0);
-    let [lastSearch, setLastSearch] = useState('')
+    let [totalPages, setTotalPages] = useState(0);
+    let [lastSearch, setLastSearch] = useState('');
+    let [currentPage, setCurrentPage] = useState(1)
     let handleSubmit = (e) => {
         e.preventDefault()
         getMovie(e.target[0].value).then(data=>{
             setMovies(data.Search)
             //very complex math
-            setCurrentPage(Number(Math.floor(data.totalResults/10)+(data.totalResults%10&&1)))
+            setTotalPages(Number(Math.floor(data.totalResults/10)+(data.totalResults%10&&1)))
+            setCurrentPage(1)
             setLastSearch(e.target[0].value)
         })
         
     }
     let handleChange = (page) => {
         getMovie(lastSearch, page).then(data=>setMovies(data.Search))
+        setCurrentPage(page)
     }
     return (
         <>
@@ -31,7 +34,7 @@ function Movies(){
             <div className="cards-container">
                 {movies&&movies.map((movie, index)=><Movie key={index} movie={movie}/>)}
             </div>
-            {currentPage>0&&<Pagination count={currentPage} onChange={(e, page)=>handleChange(page)} classes={{root: 'pagination'}} size="large"/>}
+            {totalPages>0&&<Pagination count={totalPages} page={currentPage} onChange={(e, page)=>handleChange(page)} classes={{root: 'pagination'}} size="large"/>}
 
         </>
     );
